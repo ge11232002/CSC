8818aae482bd1ab6d9cf87f5ba168c5de077f4ab
Ge Tan <gtan@me.com>
2013-04-25 16:34:37 +0100

    add the CNEPlotInstance modules (plugins)

--- ancora/gbrowse2/conf/plugins/CNEPlotInstance4.pm
+++ ancora/gbrowse2/conf/plugins/CNEPlotInstance4.pm
@@ -1,1 +1,12 @@
-/opt/www/ancora/conf/gbrowse.conf/plugins/CNEPlotInstance4.pm
+package Bio::Graphics::Browser::Plugin::CNEPlotInstance4;
+use strict;
+use vars '@ISA';
+#use lib '/home/engstrom/DEVEL/CNE/lib';
+use Bio::Graphics::Browser::Plugin::CNEPlot;
+@ISA = qw(Bio::Graphics::Browser::Plugin::CNEPlot);
+
+
+sub static_plugin_setting {
+    my ($self, $name) = @_;
+    return $self->browser_config->plugin_setting($name);
+}
--- ancora/gbrowse2/conf/plugins/CNEPlotInstance9.pm
+++ ancora/gbrowse2/conf/plugins/CNEPlotInstance9.pm
@@ -1,1 +1,12 @@
-/opt/www/ancora/conf/gbrowse.conf/plugins/CNEPlotInstance9.pm
+package Bio::Graphics::Browser::Plugin::CNEPlotInstance9;
+use strict;
+use vars '@ISA';
+#use lib '/home/engstrom/DEVEL/CNE/lib';
+use Bio::Graphics::Browser::Plugin::CNEPlot;
+@ISA = qw(Bio::Graphics::Browser::Plugin::CNEPlot);
+
+
+sub static_plugin_setting {
+    my ($self, $name) = @_;
+    return $self->browser_config->plugin_setting($name);
+}
