98aac02bb6117b87652f4460d398049e4a4f4eb6
Ge Tan <gtan@me.com>
2013-05-13 16:14:06 +0100

    fix the download for new format

--- ancora/cne/scripts/cne_pipeline/downloadAlignments.r
+++ ancora/cne/scripts/cne_pipeline/downloadAlignments.r
@@ -11,26 +11,30 @@ downloadUCSCPA = function(build1, build2, outputDir=NULL){
   }else{
     upperBuild2 = paste(toupper(substring(build2,1,1)), substring(build2,2), sep="")
     build1URL = paste(goldenPath, build1, "/vs", upperBuild2, "/axtNet/*", sep="")
+    build1URL2 = paste(goldenPath, build1, "/vs", upperBuild2, "/", build1, ".", build2, ".net.axt.gz", sep="")
   }
   cmd = paste("rsync -avzP", build1URL, outputDirBuild1)
   message(cmd)
-  res = try(system(cmd))
-  if(class(res) == "try-error"){
+  res1 = try(system(cmd))
+  cmd = paste("rsync -avzP", build1URL2, outputDirBuild1)
+  message(cmd)
+  res2 = try(system(cmd))
+  if(class(res) == "try-error" | class(res2) == "try-error"){
     return(paste(build1, build2))
   }else{
     return("Success")
   }
 }
 
-builds = list("hg19"=c("mm10", "xenTro3", "tetNig2", "canFam3", "galGal4", "danRer7", "fr3", "danRer6", "equCab2", "danRer5", "anoCar2"),
+builds = list("hg19"=c("mm10", "xenTro3", "tetNig2", "canFam3", "galGal3", "danRer7", "fr3", "danRer6", "equCab2", "danRer5", "anoCar2"),
               "hg18"=c("mm9", "ornAna1", "danRer5", "canFam2", "galGal3", "tetNig1", "monDom4", "xenTro2"),
               "mm10"=c("hg19", "galGal4", "danRer7", "fr3", "danRer6"),
               "mm9"=c("hg18", "canFam3", "equCab2", "danRer7", "danRer6", "danRer5", "tetNig2"),
               "canFam2"=c("hg18", "equCab2"),
               "canFam3"=c("hg19", "mm9"),
               "equCab2"=c("hg19", "mm9", "galGal3", "canFam2"),
-              "galGal3"=c("hg18", "equCab2"),
-              "galGal4"=c("mm10", "hg19"),
+              "galGal3"=c("hg19", "hg18", "equCab2"),
+              "galGal4"=c("mm10"),
               "danRer7"=c("hg19", "mm10", "mm9", "oryLat2", "fr3", "tetNig2", "gasAcu1"),
               "danRer6"=c("hg19", "mm10", "mm9", "oryLat2", "tetNig2", "gasAcu1"),
               "danRer5"=c("hg19", "hg18", "mm9", "oryLat2", "tetNig1", "fr2"),
