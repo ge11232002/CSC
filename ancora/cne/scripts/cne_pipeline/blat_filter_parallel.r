adf945062b93c4fdc14a015b7e674ca7de4221d2
Ge Tan <gtan@me.com>
2013-05-15 19:39:45 +0100

    fix the tmp directory in parallel

--- ancora/cne/scripts/cne_pipeline/blat_filter_parallel.r
+++ ancora/cne/scripts/cne_pipeline/blat_filter_parallel.r
@@ -9,10 +9,10 @@ files = list.files(preBlatFilter, full.names=TRUE)
 
 library(multicore)
 
-cmd = "perl /opt/www/cne/scripts/cne_pipeline/blat_filter.pl"
+cmd = "perl /opt/www/cne/scripts/cne_pipeline/blat_filter.pl --tmp "
 
 report = mclapply(files, function(oneFile){
-                  res=try(system(paste(cmd, oneFile))); 
+                  res=try(system(paste(cmd, Sys.getpid(),"-tmp ", oneFile, sep=""))); 
                   if(res != 0){return(oneFile)}}, mc.cores=nr_cores)
 
 
