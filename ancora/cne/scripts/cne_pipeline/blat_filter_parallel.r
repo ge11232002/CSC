1e1e1e00859114e6485daaa62e4d9db394842aa0
Ge Tan <gtan@me.com>
2013-05-16 11:22:01 +0100

    fix the blat_parallel

--- ancora/cne/scripts/cne_pipeline/blat_filter_parallel.r
+++ ancora/cne/scripts/cne_pipeline/blat_filter_parallel.r
@@ -1,17 +1,20 @@
 preBlatFilter = "/export/data/CNEs/pre-blatFilter"
 blatFilter = "/export/data/CNEs/blatFiltered"
 
-nr_cores = 4
+nr_cores = 24
 
-preBlatFilter = "/export/data/CNEs/hg19/annotation"
 
-files = list.files(preBlatFilter, full.names=TRUE)
+files = list.files(preBlatFilter, pattern="^cne2w_.*", full.names=TRUE)
+filesExist = list.files(blatFilter, pattern="^cne2wBf_.*")
+filesNotToCompute = sub("Bf", "", filesExist)
+files = setdiff(files, file.path(preBlatFilter, filesNotToCompute))
 
 library(multicore)
 
 cmd = "perl /opt/www/cne/scripts/cne_pipeline/blat_filter.pl --tmp "
-
+setwd(blatFilter)
 report = mclapply(files, function(oneFile){
+                  message("Doing ", oneFile);
                   res=try(system(paste(cmd, Sys.getpid(),"-tmp ", oneFile, sep=""))); 
                   if(res != 0){return(oneFile)}}, mc.cores=nr_cores)
 
