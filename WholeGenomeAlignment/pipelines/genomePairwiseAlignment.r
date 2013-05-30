903c1adae95a7c76f260d021c102e8bd688e56f5
Ge Tan <gtan@me.com>
2013-05-30 19:09:56 +0100

    refactor

--- WholeGenomeAlignment/pipelines/genomePairwiseAlignment.r
+++ WholeGenomeAlignment/pipelines/genomePairwiseAlignment.r
@@ -16,7 +16,11 @@ validchrsTarget = grep("chr", seqnames(seqinfo(TwoBitFile(assemblyTarget))), val
 validchrsTarget = grep("_", validchrsTarget, invert=TRUE, value=TRUE)
 validchrsQuery = grep("chr", seqnames(seqinfo(TwoBitFile(assemblyQuery))), value=TRUE)
 validchrsQuery = grep("_", validchrsQuery, invert=TRUE, value=TRUE)
-reg = makeRegistry(id="lastz", seed=123, file.dir=file.path(getwd(), "lastz_batchjobs"), work.dir=getwd(), skip=FALSE)
+
+## To check the command of lastz
+#lavs = lastz(assemblyTarget=assemblyTarget, assemblyQuery=assemblyQuery, chrsTarget="chr1", chrsQuery="chr1", distance=distance, format="lav", echoCommand=TRUE)
+
+reg = makeRegistry(id="lastz", seed=123, file.dir=file.path(getwd(), paste(basename(assemblyQuery), "-lastz_batchjobs", sep="")), work.dir=getwd(), skip=FALSE)
 batchExpandGrid(reg, function(chrsTarget, chrsQuery, assemblyTarget, assemblyQuery, distance, format){
                 selfDir = "~/Repos/genomics"
                 selfScripts = list.files(path=selfDir, pattern='.*\\.r', full.names=TRUE, recursive=TRUE)
@@ -31,30 +35,12 @@ batchExpandGrid(reg, function(chrsTarget, chrsQuery, assemblyTarget, assemblyQue
                 )
 submitJobs(reg)
 showStatus(reg)
+
+## Rerun the jobs automatically which expire for some reasons...Usually it happens when it hits the wall clock. But seems not to be the case on alpha.
+monitorBatchJobs(reg)
+
 lavs = reduceResultsVector(reg, fun=function(job, res) res)
-#lavs = lastz(assemblyTarget=assemblyTarget, assemblyQuery=assemblyQuery, chrsTarget="chr1", chrsQuery="chr1", distance=distance, format="lav", echoCommand=TRUE)
-filesNotCompleted = validateLastz(lavs)
-stopifnot(is.null(filesNotCompleted))
+stopifnot(length(lavs) == showStatus(reg)[1,"started"])
 
-### step 2: Chaining 
-psls = lavToPsl(lavs, removeLav=TRUE)
-chains = axtChain(psls, assemblyTarget, assemblyQuery, format="psl", distance=distance, removePsl=TRUE)
-allChain = chainMergeSort(chains, assemblyTarget, assemblyQuery, removeChains=TRUE)
-# now , this allChain is what we get from UCSC download e.g. hg19.danRer7.all.chain.gz
-
-### step 3: Netting 
-allPreChain = chainPreNet(allChain, assemblyTarget, assemblyQuery, removeAllChain=FALSE)
-# combine the chains into nets, add the synteny information to the net:
-netSyntenicFile = chainNetSyntenic(allPreChain, assemblyTarget, assemblyQuery)
-
-#  Add classification information using the database tables:
-# actually this step is not necessary in this pipeline according to http://blog.gmane.org/gmane.science.biology.ucscgenome.general/month=20130301. The class information will only be used for Genome Browser.
-# Since it needs some specific modification of the table names for certain species, we skip this step now.
-# If this step is done, then the generated class.net is the gzipped net file that you see in UCSC Downloads area.
-
-
-### step 4: axtNet
-axtFile = netToAxt(netSyntenicFile, allPreChain, assemblyTarget, assemblyQuery)
-
-
+axtNetWholePipeline(lavs, assemblyTarget, assemblyQuery, distance)
 
