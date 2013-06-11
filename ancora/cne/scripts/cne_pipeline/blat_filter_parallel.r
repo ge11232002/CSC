3e7c313742e4b07125bd52a2515eee3fa82b48e3
Ge Tan <gtan@me.com>
2013-06-11 10:55:35 +0100

    improvement to detect which files to compute

--- ancora/cne/scripts/cne_pipeline/blat_filter_parallel.r
+++ ancora/cne/scripts/cne_pipeline/blat_filter_parallel.r
@@ -7,6 +7,8 @@ nr_cores = 8
 files = list.files(preBlatFilter, pattern="^cne2w_.*", full.names=TRUE)
 filesExist = list.files(blatFilter, pattern="^cne2wBf_.*")
 filesNotToCompute = sub("Bf", "", filesExist)
+filesNotToCompute = sub("-tmp$", "", filesNotToCompute)
+
 files = setdiff(files, file.path(preBlatFilter, filesNotToCompute))
 
 library(multicore)
@@ -15,7 +17,7 @@ cmd = "perl /opt/www/cne/scripts/cne_pipeline/blat_filter.pl --tmp "
 setwd(blatFilter)
 report = mclapply(files, function(oneFile){
                   message("Doing ", oneFile);
-                  res=try(system(paste(cmd, Sys.getpid(),"-tmp ", oneFile, sep=""))); 
+                  res=try(system(paste0(cmd, basename(oneFile),"-tmp ", oneFile))); 
                   if(res != 0){return(oneFile)}}, mc.cores=nr_cores)
 
 
