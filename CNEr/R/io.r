3784da6066fafd1f5b62e67aa9d45974de389225
Ge Tan <gtan@me.com>
2013-06-27 11:38:02 +0100

    add the Biostrings's way

--- CNEr/R/io.r
+++ CNEr/R/io.r
@@ -67,6 +67,8 @@ readBedToGRanges = function(bedFile=NULL){
 #    2.272   0.133   2.414
 
 # axtFiles = c("/export/downloads/ucsc/axtNet/hg19/chr2.hg19.danRer7.net.axt.gz", "/export/downloads/ucsc/axtNet/hg19/chr3.hg19.danRer7.net.axt.gz", "/export/downloads/ucsc/axtNet/hg19/chr4.hg19.danRer7.net.axt.gz")
+# axtFiles = list.files(path="/export/downloads/ucsc/axtNet/hg19", pattern=".*hg19\\.mm10*", full.names=TRUE)
+# axt = readAxt(axtFiles)
 readAxt = function(axtFiles){
   # Read axt files into R axt object
   index_noexists = !file.exists(axtFiles)
@@ -92,3 +94,95 @@ readAxt = function(axtFiles){
             )
   return(axt)
 }
+##-----------------------------------IO from Biostrings----------------------------
+.normargInputFilepath <- function(filepath)
+{
+    if (!is.character(filepath) || any(is.na(filepath)))
+        stop("'filepath' must be a character vector with no NAs")
+    ## First pass: expand local paths and download any remote file.
+    filepath2 <- character(length(filepath))
+    for (i in seq_len(length(filepath))) {
+        fp <- filepath[i]
+        con <- file(fp)
+        con_class <- class(con)[1L]
+        close(con)
+        if (con_class == "url") {
+            filepath2[i] <- tempfile()
+            download.file(fp, filepath2[i])
+        } else {
+            filepath2[i] <- path.expand(fp)
+        }
+    }
+    ## Second pass: check the type of the local files (all files are
+    ## now local).
+    filetype <- character(length(filepath2))
+    for (i in seq_len(length(filepath2))) {
+        fp <- filepath2[i]
+        con <- file(fp)
+        ## Ugly trick to get the type of 'con'. Is there a better way?
+        filetype[i] <- showConnections(TRUE)[as.character(con), "class"]
+        close(con)
+        if (filetype[i] != "file")
+            stop("file \"", filepath[i], "\" ",
+                 "has unsupported type: ", filetype[i])
+    }
+    names(filepath2) <- filetype
+    filepath2
+}
+
+.ExternalFilePtr.close <- function(x)
+    .Call("ExternalFilePtr_close", x)
+
+### Returns a list of "external file pointers".
+.openInputFiles <- function(filepath)
+{
+    filepath2 <- .normargInputFilepath(filepath)
+    ans <- lapply(filepath2,
+           function(fp)
+           {
+               efp <- .Call("new_input_ExternalFilePtr", fp)
+               reg.finalizer(efp, .ExternalFilePtr.close, onexit=TRUE)
+               efp
+           })
+    names(ans) <- filepath
+    ans
+}
+
+### 'efp_list' must be a list of "external file pointers" returned by
+### .openInputFiles() or .openOutputFiles().
+.closeFiles <- function(efp_list)
+{
+    for (efp in efp_list) .ExternalFilePtr.close(efp)
+}
+
+.normargNrec <- function(nrec)
+{
+    if (!isSingleNumber(nrec))
+        stop("'nrec' must be a single integer value")
+    if (!is.integer(nrec))
+        nrec <- as.integer(nrec)
+    nrec
+}
+
+.normargSkip <- function(skip)
+{
+    if (!isSingleNumber(skip))
+        stop("'skip' must be a single integer value")
+    if (!is.integer(skip))
+        skip <- as.integer(skip)
+    if (skip < 0L)
+        stop("'skip' cannot be negative")
+    skip
+}
+
+axt.info = function(filepath, nrec=-1L, skip=0L, use.names=TRUE, seqtype="B"){
+  efp_list <- .openInputFiles(filepath)
+  on.exit(.closeFiles(efp_list))
+  nrec <- .normargNrec(nrec)
+  skip <- .normargSkip(skip)
+  use.names <- normargUseNames(use.names)
+  seqtype <- match.arg(seqtype, c("B", "DNA", "RNA", "AA"))
+  lkup <- get_seqtype_conversion_lookup("B", seqtype)
+  .Call("axt_info", efp_list, nrec, skip, use.names, lkup)
+}
+
--- CNEr/R/utils.r
+++ CNEr/R/utils.r
@@ -24,6 +24,46 @@ seqToAlignment = function(DNAStringSet){
 }
 
 
+###
+### We register the old-style (a.k.a. S3) class below as a formally defined
+### class (a.k.a. S4) because we are using it in some method signatures.
+### Note that dispatch still works without this registration but causes
+### 'R CMD INSTALL' to (gently) complain.
+###
+
+setOldClass("probetable")
+
+
+###
+### Some low-level (not exported) helper functions.
+###
+
+isNumericOrNAs <- function(x)
+{
+    is.numeric(x) || (is.atomic(x) && is.vector(x) && all(is.na(x)))
+}
+
+normargUseNames <- function(use.names)
+{
+    if (is.null(use.names))
+        return(TRUE)
+    if (!isTRUEorFALSE(use.names))
+        stop("'use.names' must be TRUE or FALSE")
+    use.names
+}
+
+### Returns an integer vector.
+pow.int <- function(x, y)
+{
+    if (!is.numeric(x))
+        stop("'x' must be a numeric vector")
+    if (!is.integer(x))
+        x <- as.integer(x)
+    ans <- rep.int(1L, length(x))
+    for (i in seq_len(y))
+        ans <- ans * x
+    ans
+}
 
 
 
