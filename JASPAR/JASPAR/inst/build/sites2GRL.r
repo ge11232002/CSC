570ca59a98f7a10146d75530f3b1987216d48332
Ge Tan <gtan@me.com>
2013-07-02 16:24:04 +0100

    the build script for sites2DNAStringSet

---
+++ JASPAR/JASPAR/inst/build/sites2GRL.r
@@ -0,0 +1,16 @@
+
+
+
+# filepath = "~/work/projects/JASPAR/dataPackage/sites"
+sites2GRL = function(filepath){
+  require(Biostrings)
+  files = list.files(path=filepath, pattern="*\\.sites$", full.names=TRUE)
+  GRL = list()
+  oneFile = files[1]
+  for(oneFile in files){
+    sites = readDNAStringSet(filepath=oneFile, format="fasta")
+    GRL = c(GRL, sites)
+  }
+  names(GRL) = sub("\\.sites$", "", basename(files))
+  return(GRL)
+}
