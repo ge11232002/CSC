ffc3e3bac1246c97ba3ff802e488ddd06a0215b6
Ge Tan <gtan@me.com>
2013-07-04 16:39:23 +0100

    add the show methods for axt class

--- CNEr/R/class.r
+++ CNEr/R/class.r
@@ -95,19 +95,94 @@ setMethod("[", "axt",
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### "show" method.
 ###
+### 'x' must be an XString or MaskedXString object.
+toSeqSnippet <- function(x, width)
+{
+    if (width < 7L)
+        width <- 7L
+    seqlen <- length(x)
+    if (seqlen <= width) {
+        as.character(x)
+    } else {
+        w1 <- (width - 2) %/% 2
+        w2 <- (width - 3) %/% 2
+        paste(as.character(subseq(x, start=1, width=w1)),
+              "...",
+              as.character(subseq(x, end=seqlen, width=w2)),
+              sep="")
+    }
+}
 
-#showAxt = function(x, margin=""){
-#  lx = length(x)
-#  cat(class(x), " with ",
-#      lx, " ", ifelse(lx == 1L, "alignment pair", "alignment pairs"),
-#                  ":\n", sep="")
-#    out = makePrettyMatrixForCompactPrintingAxt(x, .makeNakedMatFromAxt)
-#    if(nrow(out) != 0L)
-#          rownames(out) = paste0(margin, rownames(out))
-#      print(out, quote=FALSE, right=TRUE)
-#}
-#
-#setMethod("show", "axt",
-#          function(object)
-#            showAxt(object, margin="  ")
-#)
+
+.axt.show_frame_line = function(x, i, iW, tNameW, tStartW, tEndW, qNameW, qStartW, qEndW, scoreW){
+  cat(format(i, width=iW, justify="right"), " ",
+      format(as.vector(seqnames(targetRanges(x)[i])), width=tNameW, justify="right"), " ",
+      format(start(targetRanges(x)[i]), width=tStartW, justify="right"), " ",
+      format(end(targetRanges(x)[i]), width=tEndW, justify="right"), " ",
+      format(as.vector(seqnames(queryRanges(x)[i])), width=qNameW, justify="right"), " ",
+      format(start(queryRanges(x)[i]), width=qStartW, justify="right"), " ",
+      format(end(queryRanges(x)[i]), width=qEndW, justify="right"), " ",
+      format(as.vector(strand(queryRanges(x))[i]), width=1, justify="right"), " ",
+      format(score(x)[i], width=scoreW, justify="right"), " ",
+      sep=""
+      )
+  cat("\n")
+  snippetWidth = getOption("width")
+  seq_snippet = toSeqSnippet(targetSeqs(x)[[i]], snippetWidth)
+  cat(seq_snippet)
+  cat("\n")
+  seq_snippet = toSeqSnippet(querySeqs(x)[[i]], snippetWidth)
+  cat(seq_snippet)
+  cat("\n")
+}
+
+showAxt = function(x, margin="", half_nrow=5L){
+  lx = length(x)
+  if(is.null((head_nrow = getOption("showHeadLines"))))
+    head_nrow = half_nrow
+  if(is.null((tail_nrow = getOption("showTailLines"))))
+    tail_nrow = half_nrow
+  iW = nchar(as.character(lx))
+  tNameW = max(nchar(as.vector(seqnames(targetRanges(x)[c(1:head_nrow, (lx-tail_nrow+1L):lx)]))))
+  tStartW = max(nchar(as.character(start(targetRanges(x)[c(1:head_nrow, (lx-tail_nrow+1L):lx)]))))
+  tEndW = max(nchar(as.character(end(targetRanges(x)[c(1:head_nrow, (lx-tail_nrow+1L):lx)]))))
+  qNameW = max(nchar(as.vector(seqnames(queryRanges(x)[c(1:head_nrow, (lx-tail_nrow+1L):lx)]))))
+  qStartW = max(nchar(as.character(start(queryRanges(x)[c(1:head_nrow, (lx-tail_nrow+1L):lx)]))))
+  qEndW = max(nchar(as.character(end(queryRanges(x)[c(1:head_nrow, (lx-tail_nrow+1L):lx)]))))
+  scoreW = max(nchar(as.character(score(x)[c(1:head_nrow, (lx-tail_nrow+1L):lx)])))
+  if(lx < (2*half_nrow+1L) | (lx < (head_nrow+tail_nrow+1L))) {
+    for(i in seq_len(lx))
+      .axt.show_frame_line(x, i, iW, tNameW, tStartW, tEndW, qNameW, qStartW, qEndW, scoreW)
+  }else{
+    if(head_nrow > 0)
+      for(i in 1:head_nrow)
+        .axt.show_frame_line(x, i, iW, tNameW, tStartW, tEndW, qNameW, qStartW, qEndW, scoreW)
+    cat(format("...", width=iW, justify="right"),
+        format("...", width=tNameW, justify="right"),
+        format("...", width=tStartW, justify="right"),
+        format("...", width=tEndW, justify="right"),
+        format("...", width=qNameW, justify="right"),
+        format("...", width=qStartW, justify="right"),
+        format("...", width=qEndW, justify="right"),
+        format("...", width=scoreW, justify="right")
+        )
+    cat("\n")
+    if(tail_nrow > 0)
+      for(i in (lx-tail_nrow+1L):lx)
+        .axt.show_frame_line(x, i, iW, tNameW, tStartW, tEndW, qNameW, qStartW, qEndW, scoreW)
+  }
+}
+    #out = makePrettyMatrixForCompactPrintingAxt(x, .makeNakedMatFromAxt)
+    #if(nrow(out) != 0L)
+    #      rownames(out) = paste0(margin, rownames(out))
+    #  print(out, quote=FALSE, right=TRUE)
+
+setMethod("show", "axt",
+          function(object){
+            cat(" A ", class(object), " with ", length(object), " ", 
+                ifelse(lx == 1L, "alignment pair", "alignment pairs"), ":\n", sep="")
+            if(length(object) != 0){
+              showAxt(object, margin="  ")
+            }
+          }
+)
