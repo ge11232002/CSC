a0380cd180bcc1dee8e9f6578d3bf8b980bbc227
Ge Tan <gtan@me.com>
2013-07-17 11:22:06 +0100

    clean and move the struct definition to CNEr.h

--- CNEr/src/CNEr.h
+++ CNEr/src/CNEr.h
@@ -10,3 +10,40 @@
 #include <string.h>
 #include "IRanges_interface.h"
 
+
+
+struct range
+/* Start and end coordinate pair */
+{
+  int start;    /* Start (0 based) */
+  int end;    /* End (non-inclusive) */
+};
+
+struct rangeArray
+/* Array of start and end coordinate pairs */
+{
+  int n;
+  struct range *ranges;
+};
+
+struct slRange
+/* Start and end coordinate pair as linked list item */
+{
+  struct slRange *next;
+  int start;    /* Start (0 based) */
+  int end;    /* End (non-inclusive) */
+};
+
+
+/* Data structure used to represent different thresholds and intermediate results for each */
+
+struct slThreshold
+{
+  struct slThreshold *next;
+  int minScore;
+  int winSize;
+  int ceStart;
+  int ceEnd;
+  FILE *outFile;
+};
+
