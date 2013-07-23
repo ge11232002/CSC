6e9edc045d5c624e5bb5386d57139a30966c390b
Ge Tan <gtan@me.com>
2013-07-23 18:09:12 +0100

    add the function bin_ranges_from_coord_range

--- CNEr/src/utils.c
+++ CNEr/src/utils.c
@@ -1,7 +1,15 @@
 #include "CNEr.h"
 
+// This is borrowed from Jim Kend's source, binRange.c.
+/* add one new level to get coverage past chrom sizes of 512 Mb
+ *  *  effective limit is now the size of an integer since chrom start
+ *   *  and end coordinates are always being used in int's == 2Gb-1 */
+static int binOffsetsExtended[] = {4096+512+64+8+1, 512+64+8+1, 64+8+1, 8+1, 1, 0};
+
+static int binOffsets[] = {512+64+8+1, 64+8+1, 8+1, 1, 0};
+
 SEXP bin_from_coord_range(SEXP starts, SEXP ends){
-//Return the bin numbers that should be assigned to a feature spanning the given range.
+//Return the bin numbers that should be assigned to a feature spanning the given ranges.
 //Here the inputs of starts and ends are 1-based
   starts = AS_INTEGER(starts);
   ends = AS_INTEGER(ends);
@@ -18,4 +26,67 @@ SEXP bin_from_coord_range(SEXP starts, SEXP ends){
   }
   UNPROTECT(1);
   return bins;
+}
+
+
+SEXP bin_ranges_from_coord_range_standard(SEXP start, SEXP end){
+  // both are 1-based
+  if(GET_LENGTH(start) != 1 || GET_LENGTH(end) != 1)
+    error("'start' and 'end' must be a single integer");
+  start = AS_INTEGER(start);
+  end = AS_INTEGER(end);
+  int startBin = INTEGER(start)[0] - 1;
+  int endBin = INTEGER(end)[0] - 1;
+  int _binFirstShift = binFirstShift();
+  int _binNextShift = binNextShift();
+  startBin >>= binFirstShift();
+  endBin >>= binFirstShift();
+  int n = ArraySize(binOffsets);
+  SEXP ans;
+  PROTECT(ans = allocMatrix(INTSXP, n, 2));
+  int *rans = INTEGER_POINTER(ans);
+  // we use a matrix to store the bin ranges for each bin level.
+  int i;
+  for(i=0; i<n; ++i){
+    rans[i] = binOffsets[i] + startBin;
+    rans[i+n] = binOffsets[i] + endBin;
+    startBin >>= binNextShift();
+    endBin >>= binNextShift();
+  }
+  UNPROTECT(1);
+  return ans;
+}
+
+SEXP bin_ranges_from_coord_range_extended(SEXP start, SEXP end){
+  // both are 1-based
+  if(GET_LENGTH(start) != 1 || GET_LENGTH(end) != 1)
+    error("'start' and 'end' must be a single integer");
+  start = AS_INTEGER(start);
+  end = AS_INTEGER(end);
+  int startBin = INTEGER(start)[0] - 1;
+  int endBin = INTEGER(end)[0] - 1;
+  startBin >>= binFirstShift();
+  endBin >>= binFirstShift();
+  int n = ArraySize(binOffsetsExtended);
+  SEXP ans;
+  PROTECT(ans = allocMatrix(INTSXP, n, 2));
+  int *rans = INTEGER_POINTER(ans);
+  int i;
+  for(i=0; i<n; ++i){
+    rans[i] = _binOffsetOldToExtended + binOffsetsExtended[i] + startBin;
+    rans[i+n] = _binOffsetOldToExtended + binOffsetsExtended[i] + endBin;
+    startBin >>= binNextShift();
+    endBin >>= binNextShift();
+  }
+  UNPROTECT(1);
+  return ans;
+}
+
+SEXP bin_ranges_from_coord_range(SEXP start, SEXP end){
+  //Return the set of bin ranges that overlap a given coordinate range. It is usually more convenient to use bin_restriction string than to use this method directly.
+  // Here, start and end are 1-based.
+  if (INTEGER(AS_INTEGER(end))[0] <= BINRANGE_MAXEND_512M)
+    return bin_ranges_from_coord_range_standard(start, end);
+  else
+    return bin_ranges_from_coord_range_extended(start, end);
 }
