7ee0eebc792d9f9642d5226563115f1aa93b7362
Ge Tan <gtan@me.com>
2013-09-03 22:30:50 +0100

    not back Site now

--- JASPAR/JASPARTools/R/Site.r
+++ JASPAR/JASPARTools/R/Site.r
@@ -39,7 +39,7 @@ setMethod("length", "Site", function(x) length(views(x)))
 ### -------------------------------------------------------------------
 ### The constructor
 ###
-Site = function(views, score, strand="*",
+newSite = function(views, score, strand="*",
                 seqname="Unknown",
                 sitesource="TFBS", primary="TF binding site",
                 pattern){
@@ -53,7 +53,6 @@ Site = function(views, score, strand="*",
 setGeneric("writeGFF3", signature="x", function(x) standardGeneric("writeGFF3"))
 setMethod("writeGFF3", "Site",
           function(x){
-            nr = length(x)
             gff = list(seqname=seqname(x),
                        source=sitesource(x),
                        feature=sitesource(x),
@@ -72,6 +71,41 @@ setMethod("writeGFF3", "Site",
             return(gff)
           }
           )
+setGeneric("writeGFF2", signature="x", function(x) standardGeneric("writeGFF2"))
+setMethod("writeGFF2", "Site",
+          function(x){
+            gff = list(seqname=seqname(x),
+                       source=sitesource(x),
+                       feature=sitesource(x),
+                       start=start(views(x)),
+                       end=end(views(x)),
+                       score=score(x),
+                       strand=strand(x),
+                       frame=".",
+                       attributes=paste(
+                                        paste("TF", paste0("\"", name(pattern(x)), "\""), sep=" "),
+                                        paste("class", paste0("\"", matrixClass(pattern(x)), "\""), sep=" "),
+                                        paste("sequence", paste0("\"", as.character(views(x)), "\""), sep=" "),
+                                        sep="; ")
+                       )
+            gff = as.data.frame(gff)
+            return(gff)
+          }
+          )
+
           
+### -----------------------------------------------------------------
+### The "show" method
+### Perhaps it is not a bad idea to show them in gff format.
+setMethod("show", "Site",
+          function(object){
+            gff = writeGFF3(object)
+            cat("An object of class", class(object), "with", 
+                length(object), "site", 
+                ifelse(length(object)==1, "sequence", "sequences"))
+            cat("\n")
+            print(gff)
+          }
+          )
 
 
