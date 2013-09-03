f7716fa59f1b6487af6ced070a6e6a785291eb61
Ge Tan <gtan@me.com>
2013-09-03 16:10:28 +0100

    update

--- JASPAR/JASPARTools/R/XMatrixList.r
+++ JASPAR/JASPARTools/R/XMatrixList.r
@@ -50,7 +50,7 @@ setClass("PWMatrixList",
 #         )
 
 setClass("ICMatrixList",
-         contains="SimpleList",
+         contains="PWMatrixList",
          representation(
                         ),
          prototype(
@@ -63,6 +63,15 @@ setClassUnion("XMatrixList", c("PFMatrixList", "ICMatrixList", "PWMatrixList"))
 ### ----------------------------------------------------------------------
 ### The accessor-like method
 ###
+setMethod("name", "XMatrixList", function(x) sapply(x, name))
+setMethod("ID", "XMatrixList", function(x) sapply(x, ID))
+setMethod("matrixClass", "XMatrixList", function(x) sapply(x, matrixClass))
+setMethod("Matrix", "XMatrixList", function(x) lapply(x, Matrix))
+setMethod("strand", "XMatrixList", function(x) sapply(x, strand))
+setMethod("bg", "XMatrixList", function(x) lapply(x, bg))
+setMethod("matrixType", "XMatrixList", function(x) sapply(x, matrixType))
+setMethod("pseudocounts", "PWMatrixList", function(x) sapply(x, pseudocounts))
+setMethod("schneider", "ICMatrixList", function(x) sapply(x, schneider))
 
 
 ### -----------------------------------------------------------------------
