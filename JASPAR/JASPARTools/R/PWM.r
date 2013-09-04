787da8dca7c3f0b1b29d77b4a28963106344c764
Ge Tan <gtan@me.com>
2013-09-04 17:51:25 +0100

    move conservation

--- JASPAR/JASPARTools/R/PWM.r
+++ JASPAR/JASPARTools/R/PWM.r
@@ -121,34 +121,6 @@ setGeneric("doSiteSearch", signature="x",
              standardGeneric("doSiteSearch")
            )
 
-calculate_conservation = function(x, windowSize, which=c("1", "2")){
-  ## This function is used to calculate the conservation profiles for a pairwise alignment.
-  # x: a DNAStringSet with length 2 holds the alignment
-  # windowSize: the smooth window size
-  # which: which seq in the alignment is computed.
-  if(windowSize %% 2 == 0){
-    warning("windows size is not even, turned into odd by -1")
-    windowSize = windowSize - 1L
-  }
-  which = match.arg(which)
-  stopifnot(length(x) == 2)
-  if(which == "1"){
-    alignedSeq1 = x[1]
-    alignedSeq2 = x[2]
-  }else{
-    alignedSeq1 = x[2]
-    alignedSeq2 = x[1]
-  }
-  stopifnot(nchar(alignedSeq1) == nchar(alignedSeq2))
-  alignedSeq1 = strsplit(as.character(alignedSeq1), "")[[1]]
-  alignedSeq2 = strsplit(as.character(alignedSeq2), "")[[1]]
-  indexGap = alignedSeq1 == "-" | alignedSeq1 == "." | alignedSeq1 == "_"
-  alignedSeq1 = alignedSeq1[!indexGap]
-  alignedSeq2 = alignedSeq2[!indexGap]
-  matches = alignedSeq1 == alignedSeq2
-  conservations = runmean(matches, k=windowSize, alg="C", endrule="mean")
-  return(conservations)
-}
 
 do_sitesearch = function(pwm, x, min.score, windowSize, cutoff, conservation){
   windowSize = as.integer(windowSize)
