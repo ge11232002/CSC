1010afe5be4bee0d5e60d7fc203b875611266256
Ge Tan <gtan@me.com>
2013-09-05 21:28:40 +0100

    add get_Matrix_by_name

--- JASPAR/JASPARTools/R/DB-methods.r
+++ JASPAR/JASPARTools/R/DB-methods.r
@@ -75,7 +75,7 @@
 }
 
 ### get_Matrix_by_ID fetches matrix data under the given ID from the database and returns a XMatrix object.
-# Returns : a TFBS::Matrix::* object; the exact type of the object depending on the second argument (allowed values are 'PFM', 'ICM', and 'PWM'); returns undef if matrix with the given ID is not found.
+# Returns : a XMatrix object; the exact type of the object depending on the second argument (allowed values are 'PFM', 'ICM', and 'PWM'); returns NA if matrix with the given ID is not found.
 # Args: 
     #ID: is a string which refers to the stable JASPAR ID (usually something like "MA0001") with or without version numbers. "MA0001" will give the latest version on MA0001, while "MA0001.2" will give the second version, if existing. Warnings will be given for non-existing matrices.
 setMethod("get_Matrix_by_ID", "character",
@@ -85,20 +85,41 @@ setMethod("get_Matrix_by_ID", "character",
             if(missing(ID))
               stop("ID needs to be specified!")
             con = dbConnect(SQLite(), x)
+            on.exit(dbDisconnect(con))
             # separate stable ID and version number
             baseID = strsplit(ID, "\\.")[[1]][1]
             version = strsplit(ID, "\\.")[[1]][2]
             if(is.na(version))
               version = as.character(.get_latest_version(con, baseID))
+            if(length(version) == 0)
+              return(NA)
             # get internal ID - also a check for validity
             int_id = as.character(.get_internal_id(con, baseID, version))
             # get matrix using internal ID
             ans = .get_Matrix_by_int_id(con, int_id, type)
-            dbDisconnect(con)
             return(ans)
           }
           )
 
-
-
+### get_Matrix_by_name fetches matrix data under the given name from the database and returns a XMatrix object.
+# Returns : a XMatrix object; the exact type of the object depending on the second argument (allowed values are 'PFM', 'ICM', and 'PWM'); returns NA if matrix with the given name is not found.
+# Notes: According to the current JASPAR5 data model, name is not necessarily a unique identifier. Also, names change over time. In the case where there are several matrices with the same name in the database, the function fetches the first one and prints a warning on STDERR. You've been warned. Some matrices have multiple versions. The function will return the latest version. For specific versions, use get_Matrix_by_ID($ID.$version)
+setMethod("get_Matrix_by_name", "character",
+          function(x, name, type="PFM"){
+            # here x is the path of SQLite db file
+            type = match.arg(type, c("PWM", "PFM", "ICM"))
+            if(missing(name))
+              stop("name needs to be specified!")
+            con = dbConnect(SQLite(), x)
+            on.exit(dbDisconnect(con))
+            sqlCMD = paste0("SELECT distinct BASE_ID  FROM MATRIX WHERE NAME='", name, "'")
+            tempTable = dbGetQuery(con, sqlCMD)
+            baseID = tempTable[["BASE_ID"]]
+            if(length(baseID) == 0)
+              return(NA)
+            if(length(baseID) > 1)
+              warning("There are ", length(baseID), " distinct stable IDs with name ", name)
+            get_Matrix_by_ID(x, baseID[1], type=type)
+          }
+          )
 
