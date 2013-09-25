b5108e3333e0321564670217d2650699631bf608
Ge Tan <gtan@me.com>
2013-09-25 12:07:03 +0100

    fix the end bug

--- CNEr/src/utils.c
+++ CNEr/src/utils.c
@@ -22,7 +22,8 @@ SEXP bin_from_coord_range(SEXP starts, SEXP ends){
   p_end = INTEGER_POINTER(ends);
   p_bin = INTEGER_POINTER(bins);
   for(i=0; i<n; i++){
-    p_bin[i] = binFromRange(p_start[i]-1, p_end[i]-1);
+    // passing the coordinates to binFromRange in 0-based for start, 1-based for end.
+    p_bin[i] = binFromRange(p_start[i]-1, p_end[i]);
   }
   UNPROTECT(1);
   return bins;
