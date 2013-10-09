b4162e9c077b3050357b89360d0e02aef4db1be2
Ge Tan <gtan@me.com>
2013-10-09 19:21:25 +0100

    remove SiteList class definition

--- JASPAR/TFBSAll/R/SiteList-class.r
+++ JASPAR/TFBSAll/R/SiteList-class.r
@@ -1,30 +1,3 @@
-### --------------------------------------------------------------
-### SiteList objects
-###
-
-setClass("SiteList",
-         contains="SimpleList",
-         representation(
-                        ),
-         prototype(
-                   elementType="Site"
-                   )
-         )
-
-### -------------------------------------------------------------
-### SiteList() constructor
-###
-SiteList = function(..., use.names=TRUE){
-  listData = list(...)
-  if(is(listData[[1]], "list"))
-    listData = listData[[1]]
-  ok = sapply(listData, is, "Site")
-  if(!all(ok))
-    stop("SiteList() only accepts Site objects!")
-  if(!use.names)
-    names(listData) = NULL
-  IRanges:::newList("SiteList", listData)
-}
 
 ### ----------------------------------------------------------------
 ### Methods
