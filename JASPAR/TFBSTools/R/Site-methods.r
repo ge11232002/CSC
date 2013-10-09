6ec3fa2dbe62085d24b0aaa31fa7fb8d88a715bf
Ge Tan <gtan@me.com>
2013-10-09 19:29:11 +0100

    move

--- JASPAR/TFBSTools/R/Site-methods.r
+++ JASPAR/TFBSTools/R/Site-methods.r
@@ -138,3 +138,18 @@ setMethod("relScore", "Site",
           }
           )
 
+### ----------------------------------------------------------------
+### SiteList Methods
+###
+setMethod("writeGFF3", "SiteList",
+          function(x){
+            ans = do.call(rbind, lapply(x, writeGFF3))
+            return(ans)
+          }
+          )
+setMethod("writeGFF2", "SiteList",
+           function(x){
+             ans = do.call(rbind, lapply(x, writeGFF2))
+             return(ans)
+           }
+           )
