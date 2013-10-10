a745e1e57772396ccc237060ec447ff415a9fcbd
Ge Tan <gtan@me.com>
2013-10-10 17:02:09 +0100

    fix the coercion back

--- JASPAR/TFBSTools/R/coercion-methods.r
+++ JASPAR/TFBSTools/R/coercion-methods.r
@@ -1,3 +1,11 @@
+### -----------------------------------------------------------------
+### XMatrix Coercion
+###
+setAs("XMatrix", "matrix", function(from) Matrix(from))
+setAs("matrix", "PFMatrix", function(from) PFMatrix(matrix=from))
+setMethod("as.matrix", "PFMatrix",
+          function(x){Matrix(x)}
+          )
 
 ### -----------------------------------------------------------------
 ### XMatrixList Coercion
