47fc819c6dedf3e1c0e6bf457facc4e465a0e7e4
Ge Tan <gtan@me.com>
2013-10-28 12:23:45 +0000

    use the results from C version

--- CNEr/inst/scripts/usePrecomputed.r
+++ CNEr/inst/scripts/usePrecomputed.r
@@ -3,11 +3,15 @@
 dbName = "/mnt/biggley/home/gtan/work/projects/CNEr/CNErData/cne.sqlite"
 
 cneTables = list.files("/export/data/CNEs/blatFiltered_19-07-2013", 
-                       pattern=".*canFam3.*hg19.*", 
+                       pattern=".*danRer7.*tetNig2.*", 
                        full.names=TRUE)
-
+con = dbConnect(SQLite(), dbName)
+existingTables = dbListTables(con)
+dbDisconnect(con)
 cneTable = cneTables[1]
 for(cneTable in cneTables){
+  if(cneTable %in% existingTables)
+    next
   df = read.table(cneTable, header=FALSE, sep="\t", as.is=TRUE)
   df = df[ ,1:9]
   colnames(df) = c("chr1", "start1", "end1", "chr2", "start2", "end2", "strand", "similarity", "cigar")
