4140a2f22d0060fe0cf1cbb39c0d1f47aefe92d5
Ge Tan <gtan@me.com>
2013-10-30 22:12:11 +0000

    update

--- JASPAR/TFBSTools/src/matrixAligner.c
+++ JASPAR/TFBSTools/src/matrixAligner.c
@@ -30,8 +30,11 @@ struct alignment
 
 /* ----------------.Call() Entry points: the main matrixAligner function ------------- */
 SEXP matrixAligner(SEXP matrixQuery, SEXP matrixSubject, SEXP open_penalty, SEXP ext_penalty){
+  // matrixQuery and matrixSubject are matrix of integers.
+  // open_penalty and ext_penalty are numerics.
   int vidd1; // column number of matrixQuery
   int vidd2; // column number of matrixSubject
+  int i, j;
 
   vidd1 = INTEGER(GET_DIM(matrixQuery))[1];
   vidd2 = INTEGER(GET_DIM(matrixSubject))[1];
@@ -40,9 +43,43 @@ SEXP matrixAligner(SEXP matrixQuery, SEXP matrixSubject, SEXP open_penalty, SEXP
   // for simplicity with old code, but at the cost of assignment
   // make another matrix of profile with additional column so make pos 1 is index 1 in the matrix.
   float matris1[vidd1+1][4];
-  int position_weights[vidd1+1]; 
-  Rprintf("the position weight is %d\n", position_weights[0]); 
-  Rprintf("the element is %f\n", matris1[0][0]);
+  float matris2[vidd2+1][4];
+  float position_weights[vidd1+1]; // stores the number of sequences in each position
+  float position_weights2[vidd2+1]; 
+  
+  // fill the first row of matris1 with 0 and position_weights with 0
+  for(j=0; j<=3; j++){
+    matris1[0][j] = 0;
+    matris2[0][j] = 0;
+  }
+  for(i=0; i<=vidd1+1; i++){
+    position_weights[i] = 0;
+    position_weights2[i] = 0;
+  }
+  
+  // fill in the matrix with these data:
+  for(i=1; i<=vidd1; i++){
+    for(j=0; j<=3; j++){
+      matris1[i][j] = (float)INTEGER(matrixQuery)[(i-1)*4+j];
+      position_weights[i] += matris1[i][j];
+    }
+  }
+  for(i=1; i<=vidd2; i++){
+    for(j=0; j<=3; j++){
+      matris2[i][j] = (float)INTEGER(matrixSubject)[(i-1)*4+j];
+      position_weights2[i] += matris2[i][j];
+    }
+  }
+
+  // normalize the data
+  for(j=0; j<=3; j++){
+    for(i=1; i<=vidd1; i++){
+      matris1[i][j] = matris1[i][j] / position_weights[i];
+    }
+  }
+  Rprintf("the position weight is %f\n", matris1[1][0]);
+  Rprintf("the position weight is %f\n", matris1[1][3]);  
+
   return R_NilValue;
 
 }
