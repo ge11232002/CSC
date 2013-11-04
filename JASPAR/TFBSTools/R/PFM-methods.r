78ba6edc5f94ea93cdc6de88af19d3d1fcf1fc6b
Ge Tan <gtan@me.com>
2013-11-04 19:14:19 +0000

    add aligning the iUPAC

--- JASPAR/TFBSTools/R/PFM-methods.r
+++ JASPAR/TFBSTools/R/PFM-methods.r
@@ -59,4 +59,34 @@ setMethod("searchMatrix", signature(pfmSubject="PFMatrixList", pfmQuery="PFMatri
           }
           )
 
+setMethod("searchMatrix", signature(pfmSubject="matrix", pfmQuery="character"),
+          function(pfmSubject, pfmQuery, openPenalty=3, extPenalty=0.01){
+            pfmQuery = strsplit(pfmQuery, "")[[1]]
+            if(!all(pfmQuery %in% names(IUPAC_CODE_MAP))){
+              stop("All characters must be in IUPAC_CODE_MAP!")
+            }
+            pfmQueryMatrix = matrix(0, nrow=4, ncol=length(pfmQuery),
+                                    dimnames=list(c("A", "C", "G", "T")))
+            for(i in 1:length(pfmQuery)){
+              dnaCharacters = strsplit(IUPAC_CODE_MAP[pfmQuery[i]], "")[[1]]
+              pfmQueryMatrix[dnaCharacters, i] = 1
+            }
+            searchMatrix(pfmSubject, pfmQueryMatrix,
+                         openPenalty=openPenalty, extPenalty=extPenalty)
+          }
+          )
 
+setMethod("searchMatrix", signature(pfmSubject="PFMatrix", pfmQuery="character"),
+          function(pfmSubject, pfmQuery, openPenalty=3, extPenalty=0.01){
+            searchMatrix(Matrix(pfmSubject), pfmQuery,
+                         openPenalty=openPenalty, extPenalty=extPenalty)
+          }
+          )
+                                    
+setMethod("searchMatrix", signature(pfmSubject="PFMatrixList", pfmQuery="character"),
+          function(pfmSubject, pfmQuery, openPenalty=3, extPenalty=0.01){
+            ans = lapply(pfmSubject, searchMatrix, pfmQuery,
+                         openPenalty=openPenalty, extPenalty=extPenalty)
+            return(ans)
+          }
+          )
