421a189a5e41fd99381b4167d031bb3693e21152
Ge Tan <gtan@me.com>
2013-11-28 13:22:04 +0000

    add AllGenerics.r

---
+++ CNEr/R/AllGenerics.r
@@ -0,0 +1,17 @@
+
+### -----------------------------------------------------------------
+### axt object related
+###
+setGeneric("targetRanges", function(x) standardGeneric("targetRanges"))
+setGeneric("targetSeqs", function(x) standardGeneric("targetSeqs"))
+setGeneric("queryRanges", function(x) standardGeneric("queryRanges"))
+setGeneric("querySeqs", function(x) standardGeneric("querySeqs"))
+setGeneric("symCount", function(x) standardGeneric("symCount"))
+setGeneric("subAxt", function(x, chr, start, end, strand=c("+", "-", "*"),
+                              select=c("target", "query"),
+                              type=c("any", "within")) standardGeneric("subAxt"))
+### -----------------------------------------------------------------
+### general
+###
+setGeneric("clone", function(x, ...) standardGeneric("clone"))  # not exported
+
