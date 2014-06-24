0ca8cde457b2d5a69237cbd34336eaa7b180304e
Ge Tan <gtan@me.com>
2014-06-24 19:12:23 +0100

    blat filter for slurm

--- ancora/cne/scripts/cne_pipeline/blat_filter_parallel.r
+++ ancora/cne/scripts/cne_pipeline/blat_filter_parallel.r
@@ -12,13 +12,27 @@ filesNotToCompute = sub("-tmp$", "", filesNotToCompute)
 files = setdiff(files, file.path(preBlatFilter, filesNotToCompute))
 
 library(parallel)
+library(BatchJobs)
 
 cmd = "perl /opt/www/cne/scripts/cne_pipeline/blat_filter.pl --tmp "
 setwd(blatFilter)
-report = mclapply(files, function(oneFile){
-                  message("Doing ", oneFile);
-                  res=system(paste0(cmd, basename(oneFile),"-tmp ", oneFile)); 
-                  if(res != 0){return(oneFile)}}, mc.cores=nr_cores)
+reg <- makeRegistry(id="blatfilter", seed=123,
+                    file.dir=file.path(getwd(), paste("blatfilter", 
+                                                      "-batchjobs",
+                                                      sep="")),
+                    work.dir=getwd(), skip=FALSE)
+batchMap(reg, function(oneFile, cmd){
+         res=system(paste0(cmd, basename(oneFile)," ", oneFile))
+         return(oneFile)
+                    }, files, more.args=list(cmd=cmd)
+         )
+submitJobs(reg)
+showStatus(reg)
+
+#report = mclapply(files, function(oneFile){
+#                  message("Doing ", oneFile);
+#                  res=system(paste0(cmd, basename(oneFile),"-tmp ", oneFile)); 
+#                  if(res != 0){return(oneFile)}}, mc.cores=nr_cores)
 
 
 
