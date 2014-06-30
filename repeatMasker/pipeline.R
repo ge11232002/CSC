1335af5112e1d0dc44d2c4260671a94f8a5cf0a7
Ge Tan <gtan@me.com>
2014-06-30 18:55:37 +0100

    repeatMasker pipeline

---
+++ repeatMasker/pipeline.R
@@ -0,0 +1,29 @@
+selfDir = "~/Repos/CSC/repeatMasker/scripts"
+selfScripts = list.files(path=selfDir, pattern='.*\\.R', full.names=TRUE, 
+                         recursive=TRUE)
+for(rs in selfScripts){message(rs);source(rs)}
+
+### Partition
+# perl /mnt/biggley/home/gtan/Repos/CSC/repeatMasker/scripts/src_hg_utils_automation_simplePartition.pl /export/data/goldenpath/DHAB/DHAB.unmasked.2bit 500000 RMPart
+
+### RepeatMasker
+inLstFns <- readLines("RMPart/partitions.lst")
+inLstFns <- file.path(getwd(), "RMPart", inLstFns)
+
+
+library(BatchJobs)
+reg <- makeRegistry(id="repeatMasker", seed=123,
+                    file.dir=file.path(getwd(), 
+                                       paste("repeatMasker", "-batchjobs",
+                                             sep="")),
+                    work.dir=getwd(), skip=FALSE)
+batchMap(reg, repeatMasker, inLstFns,
+         more.args=list(species="danio", engine="crossmatch",
+                        binary="/usr/local/bin/RepeatMasker/RepeatMasker")
+         )
+ids <- getJobIds(reg)
+idsChunk <- chunk(ids, chunk.size=50)
+#testJob(reg, 1)
+submitJobs(reg, ids=idsChunk)
+showStatus(reg)
+
---
+++ repeatMasker/scripts/repeatMasker.R
@@ -0,0 +1,57 @@
+
+
+
+repeatMasker <- function(inLstFn, species="danio", engine="crossmatch",
+                         binary="/usr/local/bin/RepeatMasker/RepeatMasker"){
+  engine <- match.arg(engine, c("crossmatch", "wublast", "abblast", "ncbi",
+                                "hmmer", "decypher"))
+  finalOutFn <- paste0(inLstFn, ".out")
+  inLftFn <- sub("\\.lst$", ".lft", inLstFn)
+  alignOutFn <- paste0(inLstFn, ".align")
+  catOutFn <- paste0(inLstFn, ".cat")
+
+  cwd <- getwd()
+  tmpDir <- tempdir()
+  setwd(tmpDir)
+  # Initialize local library
+  cmd <- paste(binary, "-engine", engine, "-pa 1 -species", species, "/dev/null")
+  CNEr:::my.system(cmd)
+
+  # do the repeatMasker
+  specs <- readLines(inLstFn)
+  for(spec in specs){
+    # Remove path and .2bit filename to get just the seq:start-end spec:
+    # If $spec is the whole sequence, twoBitToFa removes the :start-end part,
+    # which causes liftUp to barf later.  So tweak the header back to
+    # seq:start-end for liftUp's sake:
+    base <- sub("[^:]+:", "", spec)
+    cmd <- paste("twoBitToFa", spec, "stdout | sed -e",
+                 paste0("\"s/^>.*/>", base,"/\""), ">",
+                 paste0(base, ".fa"))
+    CNEr:::my.system(cmd)
+    
+    cmd <- paste(binary, "-engine", engine, " -pa 1 -align -species", 
+                 species, paste0(base, ".fa"))
+    CNEr:::my.system(cmd)
+    if(file.exists(paste0(base, ".fa.cat"))){
+      file.rename(paste0(base, ".fa.cat"), catOutFn)
+    }
+  }
+  # Lift up (leave the RepeatMasker header in place because we'll liftUp
+  # again later):
+  faOutFns <- list.files(path=".", pattern=".*\\.fa\\.out")
+  cmd <- paste("liftUp -type=.out stdout",  inLftFn, "error", 
+               paste0(faOutFns, collapse=" "), "> tmpOut_out")
+  alignFiles <- list.files(path=".", pattern=".*\\.align")
+  if(length(alignFiles) > 0){
+    cmd <- paste("liftUp -type=.align stdout", inLftFn, "error",
+                 paste0(alignFiles, collapse=" "), "> tmpOut_align")
+    CNEr:::my.system(cmd)
+  }
+  file.rename("tmpOut_out", finalOutFn)
+  file.rename("tmpOut_align", alignOutFn)
+  setwd(cwd)
+  unlink(tmpDir, recursive=TRUE)
+  return("success")
+}
+
