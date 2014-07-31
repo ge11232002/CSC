d65aed09a6b8eb8547a664e9b038ca98f292ff9c
Ge Tan <gtan@me.com>
2014-07-31 11:51:55 +0100

    update

--- WholeGenomeAlignment/pipelines/lastPipeline.R
+++ WholeGenomeAlignment/pipelines/lastPipeline.R
@@ -1,5 +1,6 @@
 selfDir = "~/Repos/CSC/WholeGenomeAlignment/scripts"
-selfScripts = list.files(path=selfDir, pattern='.*\\.r', full.names=TRUE, recursive=TRUE)
+selfScripts = list.files(path=selfDir, pattern='.*\\.r', full.names=TRUE, 
+                         recursive=TRUE, ignore.case=TRUE)
 for(rs in selfScripts){message(rs);source(rs)}
 
 
@@ -8,7 +9,10 @@ distance = "near"
 targetDB= "/export/data/goldenpath/DHAB/DHAB"
 assemblyQuery = "/export/data/goldenpath/danRer7/danRer7.fa"
 
-last(targetDB, assemblyQuery, outputFn="DHAB.danRer7.maf")
+last(targetDB, assemblyQuery, outputFn="DHAB.danRer7.maf",
+     distance="near", format="MAF", mc.cores=1L)
 
+## Convert maf to psl
+### maf-convert.py psl DHAB.danRer7.maf > DHAB.danRer7.psl
 
 
