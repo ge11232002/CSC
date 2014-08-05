a1e8d774f5861e0a7cbb12d601f4c6064c290633
Ge Tan <gtan@me.com>
2014-08-05 10:56:13 +0100

    update

--- WholeGenomeAlignment/pipelines/lastPipeline.R
+++ WholeGenomeAlignment/pipelines/lastPipeline.R
@@ -16,13 +16,17 @@ last(targetDB, assemblyQuery, outputFn="DHAB.danRer7.maf",
 ### maf-convert.py psl DHAB.danRer7.maf > DHAB.danRer7.psl
 
 
+## split psl
+### split --lines=9767336 DHAB.danRer7.psl
+
 ## psl to chain
 assemblyTarget = "/export/data/goldenpath/DHAB/DHAB.2bit"
 assemblyQuery = "/export/data/goldenpath/danRer7/danRer7.2bit"
 outputs = "DHAB.danRer7.chain"
-psls = "DHAB.danRer7.psl"
+psls = list.files(path=".", pattern="x.*")
+outputs = paste0(psls, ".chain")
 
 chains = axtChain(psls, assemblyTarget, assemblyQuery, format="psl",
-                  outputs=outputs, distance=distance, removePsl=removeFiles)
+                  outputs=outputs, distance=distance, removePsl=FALSE)
 
 
