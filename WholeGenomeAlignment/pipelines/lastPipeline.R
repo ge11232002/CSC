093aa24d319accb5cdb2bc7a6f630463da54da87
Ge Tan <gtan@me.com>
2014-08-05 16:03:15 +0100

    add full pipeline

--- WholeGenomeAlignment/pipelines/lastPipeline.R
+++ WholeGenomeAlignment/pipelines/lastPipeline.R
@@ -23,10 +23,23 @@ last(targetDB, assemblyQuery, outputFn="DHAB.danRer7.maf",
 assemblyTarget = "/export/data/goldenpath/DHAB/DHAB.2bit"
 assemblyQuery = "/export/data/goldenpath/danRer7/danRer7.2bit"
 outputs = "DHAB.danRer7.chain"
-psls = list.files(path=".", pattern="x.*")
-outputs = paste0(psls, ".chain")
-
+psls = list.files(path="psl", pattern="x.*")
+outputs <- file.path("chain", sub(paste("\\.", "psl", "$", sep=""),
+                                  ".chain", basename(psls), ignore.case=TRUE))
+removeFiles = FALSE
 chains = axtChain(psls, assemblyTarget, assemblyQuery, format="psl",
                   outputs=outputs, distance=distance, removePsl=FALSE)
+allChain = chainMergeSort(path="chain", assemblyTarget, assemblyQuery, removeChains=removeFiles)
+
+### step 3: Netting
+allPreChain = chainPreNet(allChain, assemblyTarget, assemblyQuery, removeAllChain=removeFiles)
+# combine the chains into nets, add the synteny information to the net:
+netSyntenicFile = chainNetSyntenic(allPreChain, assemblyTarget, assemblyQuery)
+
+### step 4: axtNet
+axtFile = netToAxt(netSyntenicFile, allPreChain, assemblyTarget, 
+                   assemblyQuery, removeFiles=removeFiles)
+### gzip the axtNet file
+my.system(paste("gzip", axtFile))
 
 
