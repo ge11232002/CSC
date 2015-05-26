79fd53401e7608bc7f3cf4a981238ff641dd5149
Ge Tan <gtan@me.com>
2015-05-26 18:52:31 +0100

    update

---
+++ ancora/cne/scripts/cne_pipeline/fa2Repeats.R
@@ -0,0 +1,18 @@
+## This script extracts the repeats coordinates from the ENSEMBL softmasked sequences.
+library(Biostrings)
+library(GenomicRanges)
+library(rtracklayer)
+
+foo = readBStringSet("Astyanax_mexicanus.AstMex102.dna_sm.nonchromosomal.fa")
+names(foo) = sapply(strsplit(names(foo), " "), "[", 1)
+foo3 = lapply(lapply(strsplit(as.character(foo),"") ,"%in%", 
+                     c("a","c","g","t")), Rle)
+foo4 = lapply(foo3, as, "IRanges")
+foo5 = GRanges(seqnames=Rle(names(foo4), sapply(foo4, length)),
+    ranges=IRanges(start=unlist(sapply(foo4, start)),
+             end=unlist(sapply(foo4, end))),
+    strand="+")
+
+export.bed(foo5, "repeats.bed")
+
+
