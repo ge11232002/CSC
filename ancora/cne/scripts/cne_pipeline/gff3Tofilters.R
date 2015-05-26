3a2597589880030ab4f3277149d4c9f5265d043c
Ge Tan <gtan@me.com>
2015-05-26 18:59:14 +0100

    update

--- ancora/cne/scripts/cne_pipeline/gff3Tofilters.R
+++ ancora/cne/scripts/cne_pipeline/gff3Tofilters.R
@@ -2,17 +2,18 @@
 library(rtracklayer)
 
 ## set
-gffFn <- "../annotation/Glossina-morsitans-Yale_BASEFEATURES_GmorY1.3.gff3_0"
-filtersFn <- "filter_regions.GmorY1.bed"
-chroms <- seqnames(seqinfo(TwoBitFile("/export/data/goldenpath/GmorY1/assembly.2bit")))
+gffFn <- "../annotation/Astyanax_mexicanus.AstMex102.80.gtf"
+filtersFn <- "filter_regions.AstMex102.bed"
+chroms <- seqnames(seqinfo(TwoBitFile("/export/data/goldenpath/AstMex102/assembly.2bit")))
 
 ## compute
-gff <- read.table(gffFn, header=FALSE, sep="\t", stringsAsFactors=FALSE, quote="")
+gff <- read.table(gffFn, header=FALSE, sep="\t", stringsAsFactors=FALSE, 
+                  quote="")
 filterFeatures = c("exon", "repeat_region", "long_terminal_repeat")
 
 gff = gff[gff$V3 %in% filterFeatures, c("V1", "V4", "V5")]
 
-gff = transform(gff, V4=V4-1)
+gff = transform(gff, V4=V4-1L)
 dim(gff)
 gff = gff[gff$V1 %in% chroms, ]
 dim(gff)
