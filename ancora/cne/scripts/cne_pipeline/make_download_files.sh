8a976df0e6653929763dbd7a3f5b08704ebc8f42
Ge Tan <gtan@me.com>
2015-06-09 19:27:32 +0100

    update

--- ancora/cne/scripts/cne_pipeline/make_download_files.sh
+++ ancora/cne/scripts/cne_pipeline/make_download_files.sh
@@ -3,4 +3,5 @@
 #perl make_download_files.pl -w 40 dm3 tetNig2
 #perl make_download_files.pl -w 20 ce10
 #perl make_download_files.pl -w 20 SC5314A21 RIB40
-perl make_download_files.pl -w 300 hg19 mm9 danRer7 calMil1
+#perl make_download_files.pl -w 300 hg19 mm9 danRer7 calMil1
+perl make_download_files.pl -w 50 Brafl2 Bl71nemr
