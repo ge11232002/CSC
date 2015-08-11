305f9d002b2139cc4c1aed3b85b516830100b150
Ge Tan <gtan@me.com>
2015-08-11 19:46:17 +0100

    fix a bug of 1-based start for bioMart

--- ancora/cne/scripts/cne_pipeline/ens4filters.r
+++ ancora/cne/scripts/cne_pipeline/ens4filters.r
@@ -1,17 +1,26 @@
 
 library(rtracklayer)
 
-build = "danRer10"
-ensemblGeneFile = "../annotation/ensembl_genes.dat"
+build = "dm6"
+ensemblGeneFile = "../annotation/ensembl_genes.tsv"
 assembly = file.path("/export/data/goldenpath", build, "assembly.2bit")
 
-ensemblGene = read.table(ensemblGeneFile, sep="\t", header=TRUE, quote="", colClass="character")
+ensemblGene = read.table(ensemblGeneFile, sep="\t", header=TRUE, 
+                         quote="", stringsAsFactors=FALSE)
 
 bed = ensemblGene[, c("Chromosome.Name", "Exon.Chr.Start..bp.", "Exon.Chr.End..bp.")]
+bed <- transform(bed, Exon.Chr.Start..bp.=Exon.Chr.Start..bp.-1L)
 
 ## for hg19, mm10, mm9, tetNig2, canFam3, galGal3, anoCar2, equCab2, ornAna1, ce10
 bed = transform(bed, Chromosome.Name=paste("chr", Chromosome.Name, sep=""))
 bed = transform(bed, Chromosome.Name=sub("chrMT", "chrM", Chromosome.Name))
+
+## dm6
+bed <- transform(bed, Chromosome.Name=sub("dmel_mitochondrion_genome", 
+                                          "M", Chromosome.Name))
+bed <- subset(bed, subset=bed$Chromosome.Name %in% 
+              c("2L", "2R", "3L", "3R", "4", "M", "X", "Y"))
+bed <- transform(bed, Chromosome.Name=paste("chr", Chromosome.Name, sep=""))
 
 ## for petMar2
 # do nothing
