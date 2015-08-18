6ae229d9b1020b54f31dc37302a0c9c56c34c192
Ge Tan <gtan@me.com>
2015-08-18 20:35:59 +0100

    add gap

---
+++ ancora/cne/scripts/cne_pipeline/gff3/gap.R
@@ -0,0 +1,30 @@
+
+library(rtracklayer)
+library(Biostrings)
+
+# fasta
+## TODO:
+fastaFn <- "danRer10.fa"
+seqlengths <- fasta.seqlengths(fastaFn)
+## We only consider the assembled normal chroms
+seqlengths <- seqlengths[grep("_", names(seqlengths), invert=TRUE)]
+
+# CpG
+mySession <- browserSession("UCSC")
+## TODO:
+genome(mySession) <- "danRer10"
+cpg.grange <- GRanges(seqnames=names(seqlengths),
+                      ranges=IRanges(start=1L,
+                                     end=seqlengths))
+tbl.cpg <- getTable(ucscTableQuery(mySession, track="gap",
+                                   range=cpg.grange, table="gap"))
+cpgGRangs <- GRanges(seqnames=tbl.cpg$chrom,
+                     ranges=IRanges(start=tbl.cpg$chromStart+1L,
+                                    end=tbl.cpg$chromEnd),
+                     strand="+",
+                     source=Rle("UCSC", nrow(tbl.cpg)),
+                     type=Rle("gap", nrow(tbl.cpg))
+                     )
+export.gff3(cpgGRangs, con="gap.gff3")
+
+
