6b4134e6a21fa3eba81d42b8098d8aac799e18eb
Ge Tan <gtan@me.com>
2015-08-18 14:53:28 +0100

    update

--- ancora/cne/scripts/cne_pipeline/gff3/processEnsemblGFF3.R
+++ ancora/cne/scripts/cne_pipeline/gff3/processEnsemblGFF3.R
@@ -14,6 +14,22 @@ gff <- txdbENS2UCSC(gff)
 
 gff <- gff[seqnames(gff) %in% names(fasta.seqlengths(fastaFn))]
 gff <- gff[which(gff$type != "chromosome")]
+gff$biotype <- NULL
+gff$description <- NULL
+gff$havana_gene <- NULL
+gff$havana_version <- NULL
+gff$logic_name <- NULL
+gff$version <- NULL
+gff$havana_transcript <- NULL
+gff$gene_id <- NULL
+gff$transcript_id <- NULL
+gff$constitutive <- NULL
+gff$ensembl_end_phase <- NULL
+gff$ensembl_phase <- NULL
+gff$exon_id <- NULL
+gff$rank <- NULL
+gff$protein_id <- NULL
+
 
 export.gff3(gff, con=sub("\\gff3$", "gbrowse.gff3", gffFn))
 
